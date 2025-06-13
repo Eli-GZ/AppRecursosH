a178daff8a0615c33895953679a5077eee812958
Elias <elias.d.gonzalez25@gmail.com>
2025-06-13 12:19:43 -0300

    update

--- recursos-humanos-react/src/empleados/ListadoEmpleados.js
+++ recursos-humanos-react/src/empleados/ListadoEmpleados.js
@@ -1,20 +1,21 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { NumericFormat } from 'react-number-format';
+import { Link } from 'react-router-dom';
 
 export default function ListadoEmpleados() {
-//URL del back-end
+  //URL del back-end
   const urlBase = "http://localhost:8080/rh-app/empleados";
 
-//hook de estado, crea una variable empleados y una funcion para modificarla setEmpleados
+  //hook de estado, crea una variable empleados y una funcion para modificarla setEmpleados
   const [empleados, setEmpleados] = useState([]);
 
-//Se llama a la funcion cargarEmpleados, obtiene los datos del back-end(array al final, provoca que se ejecute solo una vez)
+  //Se llama a la funcion cargarEmpleados, obtiene los datos del back-end(array al final, provoca que se ejecute solo una vez)
   useEffect(() => {
     cargarEmpleados();
   }, []);
 
-//get con axios. carga los datos del back-end(resultado.data) y lo guarda en empleados
+  //get con axios. carga los datos del back-end(resultado.data) y lo guarda en empleados
   const cargarEmpleados = async () => {
     const resultado = await axios.get(urlBase);
     console.log("Resultado cargar empleados");
@@ -35,6 +36,7 @@ export default function ListadoEmpleados() {
             <th scope="col">Empleado</th>
             <th scope="col">Departamento</th>
             <th scope="col">Sueldo</th>
+            <th scope="col"></th>
           </tr>
         </thead>
         <tbody>
@@ -49,6 +51,12 @@ export default function ListadoEmpleados() {
                   displayType='text'
                   thousandSeparator="," prefix='$'
                   decimalScale={2} fixedDecimalScale />
+                </td>
+                <td className='text-center'>
+                  <div>
+                    <Link to={`/editar/${empleado.idEmpleado}`}
+                    className='btn btn-primary btn-sm me-3'>Editar</Link>
+                  </div>
                 </td>
               </tr>
             ))
