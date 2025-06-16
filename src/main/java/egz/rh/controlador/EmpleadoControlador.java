7e864b4f1691e60e9aadc81319585f9c187fa488
Elias <elias.d.gonzalez25@gmail.com>
2025-06-16 17:45:53 -0300

    update

--- src/main/java/egz/rh/controlador/EmpleadoControlador.java
+++ src/main/java/egz/rh/controlador/EmpleadoControlador.java
@@ -3,12 +3,15 @@ package egz.rh.controlador;
 import egz.rh.excepcion.RecursoNoEncontradoExcepcion;
 import egz.rh.modelo.Empleado;
 import egz.rh.servicio.IEmpleadoServicio;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,25 +24,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("rh-app")
 @CrossOrigin(value = "http://localhost:3000")
 public class EmpleadoControlador {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
-    
+
     @Autowired
     private IEmpleadoServicio empleadoServ;
-    
+
     @GetMapping("/empleados")
     public List<Empleado> obtenerEmpleados() {
         List<Empleado> listaEmple = empleadoServ.listarEmpleados();
         listaEmple.forEach(empleado -> logger.info(empleado.toString()));
         return listaEmple;
     }
-    
+
     @PostMapping("/empleados")
     public Empleado agegarEmpleado(@RequestBody Empleado empleado) {
         logger.info("Empleado a agregar: " + empleado);
         return empleadoServ.guardarEmpleado(empleado);
     }
-    
+
     @GetMapping("/empleados/{id}")
     public ResponseEntity<Empleado> obtenerEmpleadoId(@PathVariable Integer id) {
         Empleado emple = empleadoServ.buscarEmpleadoId(id);
@@ -49,12 +52,12 @@ public class EmpleadoControlador {
             return ResponseEntity.ok(emple);
         }
     }
-    
+
     @PutMapping("/empleados/{id}")
     public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Integer id, @RequestBody Empleado empleadoRecibido) {
         Empleado empleado = empleadoServ.buscarEmpleadoId(id);
         if (empleado == null) {
-            throw new RecursoNoEncontradoExcepcion("ElidRecibido no existe: " + id);
+            throw new RecursoNoEncontradoExcepcion("El id recibido no existe: " + id);
         }
         empleado.setNombre(empleadoRecibido.getNombre());
         empleado.setDepartamento(empleadoRecibido.getDepartamento());
@@ -62,5 +65,18 @@ public class EmpleadoControlador {
         empleadoServ.guardarEmpleado(empleado);
         return ResponseEntity.ok(empleado);
     }
-    
+
+    @DeleteMapping(("/empleados/{id}"))
+    public ResponseEntity<Map<String, Boolean>> eliminarEmpleado(@PathVariable Integer id) {
+        Empleado empleado = empleadoServ.buscarEmpleadoId(id);
+        if (empleado == null) {
+            throw new RecursoNoEncontradoExcepcion("El id recibido no existe: " + id);
+        }
+
+        empleadoServ.eliminarEmpleado(empleado);
+        //Respuesta Json {"eliminado":"true"}
+        Map<String, Boolean> respuesta= new HashMap<>();
+        respuesta.put("eliminado",Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
+    }
 }
